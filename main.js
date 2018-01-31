1056af9ee097fa61f635a88266af798c14bf8a78
Christopher Enriquez <topher.carpediem@gmail.com>
2018-01-31 16:52:23 +0800

    Use promise based implementation

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-const websocket = new WebSocket("ws://10.24.120.224:3000?serialnumber=user1")
+let signalingServer;
+let peerConnection = null;
+
 const localView = document.getElementById('localView');
 const remoteView = document.getElementById('remoteView');
-let pc;
+const callButton = document.getElementById('call');
+
+const mediaConstraints = {
+    audio : true,
+    video : true
+}
+
 const configuration = {
     //iceServers: [{ urls: 'stun:stun.l.google.com:19302'}]
     iceServers: [
-        {
-            urls: 'stun:stun.l.google.com:19302'
-        },
         {
             urls: 'turn:10.24.120.224:3478',
             username: 'topher',
@@ -16,89 +21,193 @@ const configuration = {
     ]
 };
 
-websocket.onopen = () => {
-    console.log("Connected to the signaling server")
-    navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true
+
+    signalingServer = new WebSocket('ws://10.24.120.224:3000?serialnumber=user2')
+
+    signalingServer.onopen = () => {
+        console.log('>>> Connected to the signaling server')
+    }
+
+    signalingServer.onmessage = message => {
+        console.log(`>>> Message recieved: ${message.data}`)   
+        let msg = JSON.parse(message.data)
+
+        switch(msg.type){
+            case 'offer':
+                handleOffer(msg)
+                break;
+
+            case 'answer' : 
+                handleAnswer(msg);
+                break;
+
+            case 'ice-candidate':
+                handleIceCandidate(msg)
+                break;
+
+            case 'drop-call' :
+                handleDropCall(msg)
+                break;
+        }
+    }
+
+
+
+// ===============
+// Signaling handlers
+// ===============
+
+function handleOffer(msg){
+    
+    initRTCPeerConnection()
+
+    let decscription = new RTCSessionDescription(msg.sdp)
+
+    peerConnection.setRemoteDescription(decscription).then(() => {
+        return navigator.mediaDevices.getUserMedia(mediaConstraints)
     })
     .then(mediaStream => {
         localView.srcObject = mediaStream;
-        //pc.addStream(mediaStream)
+        return peerConnection.addStream(mediaStream)
+    })
+    .then(() => {
+        return peerConnection.createAnswer();
+    })
+    .then(answer => {
+        return peerConnection.setLocalDescription(answer)
+    })
+    .then(() => {
+        sendToSignalingServer({
+            to : 'user2',
+            type : 'answer',
+            message : {
+                sdp : peerConnection.localDescription
+            }
+        });
+    })
+    .catch(handleMediaDevicesError);
+}
 
-        localView.onloadedmetadata = (data) => {
-            localView.play()
-        }
+function handleAnswer(msg){
+    
+}
+
+function handleIceCandidate(msg){
+    let candidate = new RTCIceCandidate(msg.candidate)
+
+    peerConnection.addIceCandidate(candidate)
+}
+
+function handleDropCall(msg){
+    
+}
+
+function sendToSignalingServer(message){
+    signalingServer.send(JSON.stringify(message))
+}
+
+// =========================
+// Initiate Peer Connection
+// =========================
+
+function initRTCPeerConnection() {
+    console.log(">>> Initializing peer connection...")
+
+    peerConnection = new RTCPeerConnection(configuration)
+
+    peerConnection.onicecandidate = handleICECandidateEvent;
+    peerConnection.onremovestream = handleRemoveStreamEvent;
+    peerConnection.oniceconnectionstatechange = handleICEConnectionStateChangeEvent;
+    peerConnection.onicegatheringstatechange = handleICEGatheringStateChangeEvent;
+    peerConnection.onsignalingstatechange = handleSignalingStateChangeEvent;
+    peerConnection.onnegotiationneeded = handleNegotiationNeededEvent;
+    peerConnection.ontrack = handleTrackEvent;
+
+}
+
+// =========================
+// Peer Connection event handlers
+// =========================
+
+function handleICECandidateEvent(evt){
+    if(evt.candidate){
+        sendToSignalingServer({
+            to : 'user2',
+            type : 'ice-candidate',
+            message : {
+                candidate : evt.candidate
+            }
+        });
+    }
+}
+
+function handleTrackEvent(evt){
+    remoteView.srcObject = evt.stream
+}
+
+function handleRemoveStreamEvent(){
+    closePeerConnection()
+}
+
+function handleICEConnectionStateChangeEvent(){}
+
+function handleICEGatheringStateChangeEvent(){}
+
+function handleSignalingStateChangeEvent(){}
+
+function handleNegotiationNeededEvent(){
+    peerConnection.createOffer().then(offer => {
+        return peerConnection.setLocalDescription(offer)
+    })
+    .then(() => {
+        sendToSignalingServer({
+            to : 'user2',
+            type : 'offer',
+            message : {
+                sdp : peerConnection.localDescription
+            }
+        })
     })
 }
 
-websocket.onclose = (err) => {
-    console.log(err);
-}
-
-websocket.onerror = (err) => {
-    console.log(err);
-}
-
-websocket.onmessage = (message) => {
-    console.log(message.data);
-    if (!pc) {
-        start(false)
-    }
-
-    let signal = JSON.parse(message.data)
-    if (signal.sdp) {
-        pc.setRemoteDescription(new RTCSessionDescription(signal.sdp))
-    }
-    else {
-        pc.addIceCandidate(new RTCIceCandidate(signal.candidate))
-    }
-}
 
 
-function sendMessage(message) {
-    console.log(`Sending the message : ${message}`)
-    websocket.send(message.data)
-}
+// ===========================
+// Button event listener
+// ===========================
 
+callButton.addEventListener('click', evt => {
+    console.log('>>> Calling another user')
 
-function start(isCaller) {
-    pc = new RTCPeerConnection(configuration)
+    initRTCPeerConnection()
 
-    pc.onicecandidate = evt => {
-        sendMessage(JSON.stringify({ to: 'user2', message: { candidate: evt.candidate } }))
-    }
+    console.log('>>> Opening webcam and microphone')
 
-    pc.ontrack = evt => {
-        remoteView.srcObject = evt.stream;
-    };
-
-    navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true
-    })
+    navigator.mediaDevices.getUserMedia(mediaConstraints)
     .then(mediaStream => {
-        localView.srcObject = mediaStream;
-        pc.addStream(mediaStream)
-
-        localView.onloadedmetadata = (data) => {
-            localView.play()
-        }
-
-        if (isCaller === true) {
-            pc.createOffer(gotDescription)
-        }
-        else {
-            pc.createAnswer()
-                .then(answer => pc.setLocalDescription(answer))
-                .then(() => {
-                    sendMessage(JSON.stringify({ to: 'user2', message: { sdp: answer } }))
-                })
-                .catch(error => { console.log(error) })
-        }
+        localView.srcObject = mediaStream
+        peerConnection.addStream(mediaStream)
     })
-    .catch(err => {
-        console.log(err.name + " " + err.message)
-    })
+    .catch(handleMediaDevicesError)
+})
+
+
+function handleMediaDevicesError(err){
+    switch(err.name) {
+        case 'NotFoundError':
+            console.log('>>> No camera found in the device, please install one')
+            break;
+
+        default:
+            console.log('>>> Error accessing the cammera')
+            break;
+    }
 }
 
+function closePeerConnection(){
+    remoteView.srcObject = null;
+    localView.srcObject = null;
+
+    peerConnection.close()
+    peerConnection = null;
+}
